8e501a7d25f69ecce7013fd2fb376a395a151de2
Amit Erez <amiterez98@gmail.com>
2020-04-07 18:20:02 +0300

    aws

--- routes/recipes-routers.js
+++ routes/recipes-routers.js
@@ -43,6 +43,7 @@ const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: 'perfectrecipesbucket',
+    acl: 'public-read',
     metadata: function (req, file, cb) {
       cb(null, {fieldName: 'Testing META_DATA'});
     },
