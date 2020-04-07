5df5b391e24e39ff700f7b233773f06c638a8ec8
Amit Erez <amiterez98@gmail.com>
2020-04-07 15:29:39 +0300

    aws storage

--- routes/recipes-routers.js
+++ routes/recipes-routers.js
@@ -1,17 +1,22 @@
 const router = require('express').Router();
 const multer = require('multer');
 const { db } = require('../config/postgresql-setup');
+const AWS = require('aws-sdk');
 
+const S3_BUCKET = processs.env.s3BucketName;
 // const uploadPath =  __dirname + '\uploads\images'
+AWS.config.region = 'us-west-1';
 
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, 'uploads/images')
-	},
-	filename: (req, file, cb) => {
-		cb(null, file.originalname)
-	}
-});
+// const storage = multer.diskStorage({
+// 	destination: (req, file, cb) => {
+// 		cb(null, 'uploads/images')
+// 	},
+// 	filename: (req, file, cb) => {
+// 		cb(null, file.originalname)
+// 	}
+// });
+
+const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
 	if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
@@ -33,8 +38,45 @@ const upload = multer({
 //upload an image
 router.post('/uploadImage', upload.single('recipeImage'), (req, res) => {
     if(req.file) {
-        res.json(req.file.filename);
-    }
+        // res.json(req.file.filename);
+        const file = req.file;
+	    const s3FileURL = process.env.AWS_Uploaded_File_URL_LINK;
+
+	    const s3bucket = new AWS.S3({
+	      accessKeyId: process.env.awsAccessKeyID,
+	      secretAccessKey: process.env.awsSecretAccessKey,
+	      region: 'us-west-1'
+	    });
+
+	    //Where you want to store your file
+
+	    const params = {
+	      Bucket: process.env.s3BucketName,
+	      Key: file.originalname,
+	      Body: file.buffer,
+	      ContentType: file.mimetype,
+	      ACL: "public-read"
+	    };
+
+	    s3bucket.upload(params, function(err, data) {
+	      if (err) {
+	        res.status(500).json({ error: true, Message: err });
+	      } else {
+	        res.send({ data });
+	        const newFileUploaded = {
+	          description: req.body.description,
+	          fileLink: s3FileURL + file.originalname,
+	          s3_key: params.Key
+	        };
+	        // const document = new DOCUMENT(newFileUploaded);
+	        // document.save(function(error, newFile) {
+	        //   if (error) {
+	        //     throw error;
+	        //   }
+	        // });
+	      }
+	    });
+      }
     else {
     	res.json('no file uploaded')
     };
